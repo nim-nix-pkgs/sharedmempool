e7f43744abb66ccd0322cc360bb0b8eab6141471
hugosenari <hugosenari@gmail.com>
2022-03-12 04:05:21 -0300

    chore: re index sharedmempool

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''threadsafe memory pool '';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."sharedmempool-master".url = "path:./master";
+  inputs."sharedmempool-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."sharedmempool-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
